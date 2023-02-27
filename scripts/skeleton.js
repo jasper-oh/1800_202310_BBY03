00ea965cea24aedea95ee9710f9c1c4d9b8773dd
Angela Yu <ayu107@my.bcit.ca>
2023-02-26 18:32:07 -0800

    added navbars to skeleton.js file and reformatted mainPage and index page to include skeleton.js

---
+++ scripts/skeleton.js
@@ -0,0 +1,12 @@
+// this function loads the nav bar and footer into html doc
+
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('/text/nav.html'));
+    console.log($('#footerPlaceholder').load('/text/footer.html'));
+}
+loadSkeleton(); //invokes function
+
+//link to jquery at head section: <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+//copy this into navbar section of html doc: <nav id="navbarPlaceholder"></nav>
+//copy this into footer section of html doc: <nav id="footerPlaceholder"></nav>
+//link to skeleton.js script at end of body
