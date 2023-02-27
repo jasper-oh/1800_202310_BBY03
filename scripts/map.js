7ef11f9034a1d855cf9e1c7ca931589526866671
Jasper Oh <ohyj0906@gmail.com>
2023-02-27 02:18:37 -0800

    Showing Map in Search Page

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,16 @@
+console.log("Work?")
+var script = document.createElement('script');
+const GOOGLE_API = config.apiKey;
+script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API}&callback=initMap`
+script.async = true;
+
+
 window.initMap = function(){
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat:49.24609650045579 , lng :-123.00187555550586},
+    center: {lat:49.2460, lng :-123.0018},
     zoom: 10,
   });
+  
 };
+
+document.head.appendChild(script);
