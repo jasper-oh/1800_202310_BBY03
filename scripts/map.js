cd2f37c94b93b02e680e4a4bed451699152eec20
Jasper Oh <ohyj0906@gmail.com>
2023-02-27 01:19:46 -0800

    few design done

---
+++ scripts/map.js
@@ -0,0 +1,6 @@
+window.initMap = function(){
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat:49.24609650045579 , lng :-123.00187555550586},
+    zoom: 10,
+  });
+};
