614ec9d0cad581679cd37503387457262092ae60
Jasper Oh <ohyj0906@gmail.com>
2023-03-07 13:47:18 -0800

    Add function in set button in profile page

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -16,6 +16,32 @@ slider2.addEventListener("input", function () {
 // Call this function when the "logout" button is clicked
 //-------------------------------------------------
 
+function handleWeatherPreferenceSet(){
+  let tempPreference = document.getElementById("slider1").value;
+  let humidityPreference = document.getElementById("slider2").value;
+  
+  firebase.auth().onAuthStateChanged(user =>{
+      if (user){
+         console.log(user.uid); // let me to know who is the user that logged in to get the UID
+         currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+         currentUser.get().then(userDoc=>{
+             db.collection("users").doc(user.uid).update({
+              preferTemp : tempPreference,
+              preferHumid : humidityPreference,
+             }).then(()=>{
+              var userCheck = confirm("Do you want to save?");
+
+              if( userCheck == true){
+                location.href = "/main"
+              }else{
+                return;
+              }
+             })
+         })    
+     }    
+  })
+}
+
 
 function insertNameFromFirestore(){
   // to check if the user is logged in:
@@ -26,6 +52,10 @@ function insertNameFromFirestore(){
          currentUser.get().then(userDoc=>{
              //get the user name
              var userName= userDoc.data().name;
+
+             //get The user prefer Temp
+            //  var userPreferHumid.data().preferHumid;
+
              console.log(userName);
              //$("#name-goes-here").text(userName); //jquery
              document.getElementById("name-goes-here").innerText = userName;
