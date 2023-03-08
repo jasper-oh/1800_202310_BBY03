a05ece300f7ec882b432b93171bad45fe476fcc4
Angela Yu <ayu107@my.bcit.ca>
2023-03-08 14:08:57 -0800

    mainPage, added extracted weather information and location from openweather API into webpage

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,3 +1,46 @@
+//read name into mainPage after Howdy
 document.getElementById("welcomeText1").innerText = `Howdy, ${localStorage.getItem("name")}`
 
+//open weather api key
+const apiKey = '09493051d39bc31a23363d3b99bf7f81';
 
+// Get the user's current location
+navigator.geolocation.getCurrentPosition(position => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const cityElement = document.getElementById('city');
+
+    // Use the latitude and longitude coordinates to fetch the weather data from the OpenWeather API
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Extract the temperature, location, humidity from API
+            const temperature = data.main.temp;
+            const cityName = data.name;
+            const humidity = data.main.humidity;
+
+            //store city name into localStorage
+            localStorage.setItem('location', cityName);
+
+            // Convert temperature from Kelvin to Celsius
+            const celsiusTemperature = temperature - 273.15;
+
+            //display the location on the webpage
+            const cityElement = document.getElementById('city');
+            cityElement.textContent = cityName;
+
+            //display the temp on the webpage
+            const forecastTempElement = document.getElementById('forecastTemp');
+            forecastTempElement.textContent = `${celsiusTemperature.toFixed(1)}°C`;
+
+            // Display the humidity on the webpage
+            const forecastHumidityElement = document.getElementById('forecastHumidity');
+            forecastHumidityElement.textContent = `${humidity}%`;
+
+        })
+        .catch(error => console.error(error));
+}, error => {
+    console.error(error);
+});
--- public/js/searchPage.js
+++ public/js/searchPage.js
@@ -1,10 +1,10 @@
-ready( () => {
-  function ajaxGet(url, callback){
+ready(() => {
+  function ajaxGet(url, callback) {
     const xhr = new XMLHttpRequest();
-    xhr.onload = function(){
-      if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+    xhr.onload = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         callback(this.responseText)
-      }else{
+      } else {
         console.log(this.status)
       }
     }
@@ -13,8 +13,8 @@ ready( () => {
   }
 
   const scoreList = () => {
-    ajaxGet("/getScoreList" , (data) => {
-      
+    ajaxGet("/getScoreList", (data) => {
+
       document.getElementById("searchpage-comment-list").innerHTML = data;
     })
   }
@@ -26,10 +26,17 @@ ready( () => {
 // callback function declaration
 function ready(callback) {
   if (document.readyState != "loading") {
-      callback();
-      console.log("ready state is 'complete'");
+    callback();
+    console.log("ready state is 'complete'");
   } else {
-      document.addEventListener("DOMContentLoaded", callback);
-      console.log("Listener was invoked");
+    document.addEventListener("DOMContentLoaded", callback);
+    console.log("Listener was invoked");
   }
 }
+
+// Retrieve the location name from the localStorage
+const locationName = localStorage.getItem('location');
+
+//display the location into webpage
+const scoreListLocationElement = document.getElementById('scoreList-location');
+scoreListLocationElement.textContent = locationName;
