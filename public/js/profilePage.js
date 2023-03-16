6bb8918d1e78411a23b75807704004ccc8d83ac6
Jasper Oh <ohyj0906@gmail.com>
2023-03-15 18:52:23 -0700

    profile img change function implement

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -20,26 +20,26 @@ function handleWeatherPreferenceSet(){
   let tempPreference = document.getElementById("slider1").value;
   let humidityPreference = document.getElementById("slider2").value;
   
-  firebase.auth().onAuthStateChanged(user =>{
-      if (user){
-         console.log(user.uid); // let me to know who is the user that logged in to get the UID
-         currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-         currentUser.get().then(userDoc=>{
-             db.collection("users").doc(user.uid).update({
-              preferTemp : tempPreference,
-              preferHumid : humidityPreference,
-             }).then(()=>{
-              var userCheck = confirm("Do you want to save?");
-
-              if( userCheck == true){
-                location.href = "/main"
-              }else{
-                return;
-              }
-             })
-         })    
-     }    
-  })
+  if( userCheck == true){
+    firebase.auth().onAuthStateChanged(user =>{
+        if (user){
+           console.log(user.uid); // let me to know who is the user that logged in to get the UID
+           currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+          currentUser.get().then(userDoc=>{
+              db.collection("users").doc(user.uid).update({
+                preferTemp : tempPreference,
+                preferHumid : humidityPreference,
+              }).then(()=>{
+                var userCheck = confirm("Do you want to save?");
+  
+              })
+          })    
+      }    
+    })
+    location.href = "/main"
+  }else{
+    return;
+  }
 }
 
 
@@ -47,25 +47,67 @@ function insertNameFromFirestore(){
   // to check if the user is logged in:
   firebase.auth().onAuthStateChanged(user =>{
       if (user){
-         console.log(user.uid); // let me to know who is the user that logged in to get the UID
-         currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-         currentUser.get().then(userDoc=>{
-             //get the user name
-             var userName= userDoc.data().name;
+        console.log(user.uid); // let me to know who is the user that logged in to get the UID
+        currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+        currentUser.get().then(userDoc=>{
+            //get the user name
+            var userName= userDoc.data().name;
+            localStorage.setItem("name", userName);
+            $(".edit-profile-photo-btn").attr('id', user.uid)
+            $(".save-profile-img-btn").attr('id', user.uid)
 
-             //get The user prefer Temp
-             var userPreferTemp = userDoc.data().preferTemp;
-             var userPreferHumid = userDoc.data().preferHumid;
+            //get the user Img
+            var userImg = userDoc.data().userImg;
 
-             console.log(userName);
-             //$("#name-goes-here").text(userName); //jquery
-             document.getElementById("name-goes-here").innerText = userName;
-             document.getElementById("slider2").setAttribute("value", userPreferTemp);
-             document.getElementById("slider1").setAttribute("value", userPreferHumid);
-             document.getElementById('sliderValueLabel2').innerText = userPreferHumid;
-             document.getElementById('sliderValueLabel1').innerText = userPreferTemp;
-         })    
-     }    
+            //get The user prefer Temp
+            var userPreferTemp = userDoc.data().preferTemp;
+            var userPreferHumid = userDoc.data().preferHumid;
+
+            //$("#name-goes-here").text(userName); //jquery
+            document.getElementById("name-goes-here").innerText = userName;
+            document.getElementById("slider2").setAttribute("value", userPreferTemp);
+            document.getElementById("slider1").setAttribute("value", userPreferHumid);
+            document.getElementById('sliderValueLabel2').innerText = userPreferHumid;
+            document.getElementById('sliderValueLabel1').innerText = userPreferTemp;
+
+            $(".user-photo").attr("src" , userImg)
+
+        })    
+      }    
   })
 }
+
 insertNameFromFirestore()
+
+function editClick(EditUserId){
+  db.collection("users").doc(EditUserId).get().then( userDoc => {
+    var userImg = userDoc.data().userImg
+    var radioId = "#" + trimImgName(userImg)    
+    $(radioId).prop("checked" , true)
+  })
+  $("#profileModal").modal("show")
+}
+
+function saveUserProfileImg(EditUserId){
+  var getCheckedImg = $('input[name=profile]:checked').val();
+  
+  var userCheck = confirm("Do you want to save it?");
+
+  if (userCheck == true){
+    db.collection("users").doc(EditUserId).update({
+      userImg : getCheckedImg,
+    }).then(() => {
+      alert("Successfully Saved")
+      window.location.href = "/profile"
+    })
+  }else{
+    return
+  }
+}
+
+function trimImgName(name){
+  var checkRadioId1 =  name.replace("/images/" , "")
+  var checkRadioId2 =  checkRadioId1.replace(".png" , "")
+
+  return checkRadioId2;
+}
