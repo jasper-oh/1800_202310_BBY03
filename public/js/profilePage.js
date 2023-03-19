c24ffdd165abe12cf0d46dab8b110648d35ca3c7
Angela Yu <ayu107@my.bcit.ca>
2023-03-19 08:31:19 -0700

    finally fixed the logic and display the badges

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -16,210 +16,177 @@ slider2.addEventListener("input", function () {
 // Call this function when the "logout" button is clicked
 //-------------------------------------------------
 
-function handleWeatherPreferenceSet(){
+function handleWeatherPreferenceSet() {
   let tempPreference = document.getElementById("slider1").value;
   let humidityPreference = document.getElementById("slider2").value;
-  
-  if( userCheck == true){
-    firebase.auth().onAuthStateChanged(user =>{
-        if (user){
-           console.log(user.uid); // let me to know who is the user that logged in to get the UID
-           currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-          currentUser.get().then(userDoc=>{
-              db.collection("users").doc(user.uid).update({
-                preferTemp : tempPreference,
-                preferHumid : humidityPreference,
-              }).then(()=>{
-                var userCheck = confirm("Do you want to save?");
-  
-              })
-          })    
-      }    
+
+  if (userCheck == true) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(user.uid); // let me to know who is the user that logged in to get the UID
+        currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+        currentUser.get().then(userDoc => {
+          db.collection("users").doc(user.uid).update({
+            preferTemp: tempPreference,
+            preferHumid: humidityPreference,
+          }).then(() => {
+            var userCheck = confirm("Do you want to save?");
+
+          })
+        })
+      }
     })
     location.href = "/main"
-  }else{
+  } else {
     return;
   }
 }
 
 
-function insertNameFromFirestore(){
+function insertNameFromFirestore() {
   // to check if the user is logged in:
-  firebase.auth().onAuthStateChanged(user =>{
-      if (user){
-        console.log(user.uid); // let me to know who is the user that logged in to get the UID
-        currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-        currentUser.get().then(userDoc=>{
-            //get the user name
-            var userName= userDoc.data().name;
-            localStorage.setItem("name", userName);
-            $(".edit-profile-photo-btn").attr('id', user.uid)
-            $(".save-profile-img-btn").attr('id', user.uid)
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log(user.uid); // let me to know who is the user that logged in to get the UID
+      currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+      currentUser.get().then(userDoc => {
+        //get the user name
+        var userName = userDoc.data().name;
+        localStorage.setItem("name", userName);
+        $(".edit-profile-photo-btn").attr('id', user.uid)
+        $(".save-profile-img-btn").attr('id', user.uid)
 
-            //get the user Img
-            var userImg = userDoc.data().userImg;
+        //get the user Img
+        var userImg = userDoc.data().userImg;
 
-            //get The user prefer Temp
-            var userPreferTemp = userDoc.data().preferTemp;
-            var userPreferHumid = userDoc.data().preferHumid;
+        //get The user prefer Temp
+        var userPreferTemp = userDoc.data().preferTemp;
+        var userPreferHumid = userDoc.data().preferHumid;
 
-            //$("#name-goes-here").text(userName); //jquery
-            document.getElementById("name-goes-here").innerText = userName;
-            document.getElementById("slider2").setAttribute("value", userPreferTemp);
-            document.getElementById("slider1").setAttribute("value", userPreferHumid);
-            document.getElementById('sliderValueLabel2').innerText = userPreferHumid;
-            document.getElementById('sliderValueLabel1').innerText = userPreferTemp;
+        //$("#name-goes-here").text(userName); //jquery
+        document.getElementById("name-goes-here").innerText = userName;
+        document.getElementById("slider2").setAttribute("value", userPreferTemp);
+        document.getElementById("slider1").setAttribute("value", userPreferHumid);
+        document.getElementById('sliderValueLabel2').innerText = userPreferHumid;
+        document.getElementById('sliderValueLabel1').innerText = userPreferTemp;
 
-            $(".user-photo").attr("src" , userImg)
+        $(".user-photo").attr("src", userImg)
 
-        })    
-      }    
+      })
+    }
   })
 }
 
 insertNameFromFirestore()
 
-function editClick(EditUserId){
-  db.collection("users").doc(EditUserId).get().then( userDoc => {
+function editClick(EditUserId) {
+  db.collection("users").doc(EditUserId).get().then(userDoc => {
     var userImg = userDoc.data().userImg
-    var radioId = "#" + trimImgName(userImg)    
-    $(radioId).prop("checked" , true)
+    var radioId = "#" + trimImgName(userImg)
+    $(radioId).prop("checked", true)
   })
   $("#profileModal").modal("show")
 }
 
-function saveUserProfileImg(EditUserId){
+function saveUserProfileImg(EditUserId) {
   var getCheckedImg = $('input[name=profile]:checked').val();
-  
+
   var userCheck = confirm("Do you want to save it?");
 
-  if (userCheck == true){
+  if (userCheck == true) {
     db.collection("users").doc(EditUserId).update({
-      userImg : getCheckedImg,
+      userImg: getCheckedImg,
     }).then(() => {
       alert("Successfully Saved")
       window.location.href = "/profile"
     })
-  }else{
+  } else {
     return
   }
 }
 
-function trimImgName(name){
-  var checkRadioId1 =  name.replace("/images/" , "")
-  var checkRadioId2 =  checkRadioId1.replace(".png" , "")
+function trimImgName(name) {
+  var checkRadioId1 = name.replace("/images/", "")
+  var checkRadioId2 = checkRadioId1.replace(".png", "")
 
   return checkRadioId2;
 }
 
-//checking streaks 
-// Assuming that "latestSubmissionDate", "currentStreak", "oneDayStreak", "threeDayStreak", "oneWeekStreak", and "twoWeekStreak" are fields in the Firestore document
-const userRef = db.collection("users").doc(user.uid);
 
-// Get the Firestore document for the user
-userRef.get().then(userDoc => {
-  if (userDoc.exists) {
-    // Retrieve the latest submission date from the Firestore document
-    const latestSubmissionDate = userDoc.data().latestSubmissionDate.toDate();
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    //user is signed in, pass user into checkStreak
+    checkStreak(user);
+    console.log(user.uid);
 
-    // Calculate the time difference between the latest submission and the current time
-    const timeDiffInMs = Date.now() - latestSubmissionDate.getTime();
-
-    // Convert the time difference to days
-    const timeDiffInDays = Math.floor(timeDiffInMs / (1000 * 60 * 60 * 24));
-
-    // Check if the user has a current streak
-    if (timeDiffInDays <= 1) {
-      // User has a 1 day streak
-      const newStreak = userDoc.data().currentStreak + 1;
-
-      // Update the "currentStreak" field in Firestore
-      userRef.update({ currentStreak: newStreak });
-
-      // Check if the user has a 1 day streak
-      if (newStreak >= 1) {
-        // User has a 1 day streak
-        userRef.update({ oneDayStreak: true });
-      }
-
-      // Check if the user has a 3 day streak
-      if (userDoc.data().currentStreak >= 3) {
-        // User has a 3 day streak
-        userRef.update({ threeDayStreak: true });
-      }
-
-      // Check if the user has a 1 week streak
-      if (timeDiffInDays <= 7 && userDoc.data().currentStreak >= 7) {
-        // User has a 1 week streak
-        userRef.update({ oneWeekStreak: true });
-      }
-
-      // Check if the user has a 2 week streak
-      if (timeDiffInDays <= 14 && userDoc.data().currentStreak >= 14) {
-        // User has a 2 week streak
-        userRef.update({ twoWeekStreak: true });
-      }
-    } else {
-      // User does not have a current streak
-      // Update the "currentStreak" field in Firestore to 0
-      userRef.update({ currentStreak: 0 });
-    }
   } else {
-    console.log("User does not exist in Firestore.");
+    //not signed in
+    console.log("not signed in");
   }
-}).catch(error => {
-  console.log("Error getting user document: ", error);
 });
 
-//showing badge
 
-// Get the Firestore document for the user
-userRef.get().then(userDoc => {
-  if (userDoc.exists) {
-    const oneDayStreakBadge = document.getElementById("oneDayStreakBadge");
-    const threeDayStreakBadge = document.getElementById("threeDayStreakBadge");
-    const oneWeekStreakBadge = document.getElementById("oneWeekStreakBadge");
-    const twoWeekStreakBadge = document.getElementById("twoWeekStreakBadge");
 
-    // Check if the user has achieved the 1 day streak
-    if (userDoc.data().oneDayStreak) {
-      // Show the 1 day streak badge
-      oneDayStreakBadge.style.display = "inline-block";
-    } else {
-      // Hide the 1 day streak badge
-      oneDayStreakBadge.style.display = "none";
-    }
+function checkStreak(user) {
+  if (user) {
+    const ratingsRef = db.collection("ratings")
+      .where("userID", "==", user.uid)
+      .orderBy("uploadTime", "desc")
+      .get()
+      .then(querySnapshot => {
+        const ratings = querySnapshot.docs.map(doc => doc.data());
 
-    // Check if the user has achieved the 3 day streak
-    if (userDoc.data().threeDayStreak) {
-      // Show the 3 day streak badge
-      threeDayStreakBadge.style.display = "inline-block";
-    } else {
-      // Hide the 3 day streak badge
-      threeDayStreakBadge.style.display = "none";
-    }
+        let streak = 0;
+        let currentDate = new Date();
+        //iterate through every rating they posted
+        for (let i = 0; i < ratings.length; i++) {
+          const ratingDate = ratings[i].uploadTime.toDate();
+          const timeDiff = Math.floor((currentDate - ratingDate) / (1000 * 60 * 60 * 24));
+          if (timeDiff <= 1) {
+            streak++;
+            currentDate = ratingDate;
+          } else {
+            break;
+          }
+        }
 
-    // Check if the user has achieved the 1 week streak
-    if (userDoc.data().oneWeekStreak) {
-      // Show the 1 week streak badge
-      oneWeekStreakBadge.style.display = "inline-block";
-    } else {
-      // Hide the 1 week streak badge
-      oneWeekStreakBadge.style.display = "none";
-    }
+        const hasOneDayStreak = streak >= 1;
+        const hasThreeDayStreak = streak >= 3;
+        const hasOneWeekStreak = streak >= 7;
+        const hasTwoWeekStreak = streak >= 14;
 
-    // Check if the user has achieved the 2 week streak
-    if (userDoc.data().twoWeekStreak) {
-      // Show the 2 week streak badge
-      twoWeekStreakBadge.style.display = "inline-block";
-    } else {
-      // Hide the 2 week streak badge
-      twoWeekStreakBadge.style.display = "none";
-    }
+        if (hasOneDayStreak) {
+          console.log("1 day streak");
+          document.getElementById("oneDayStreakBadge").style.display = "inline-block";
+        } else {
+          document.getElementById("oneDayStreakBadge").style.display = "none";
+        }
+
+        if (hasThreeDayStreak) {
+          console.log("3 day streak");
+          document.getElementById("threeDayStreakBadge").style.display = "inline-block";
+        } else {
+          document.getElementById("threeDayStreakBadge").style.display = "none";
+        }
+
+        if (hasOneWeekStreak) {
+          console.log("1 week streak");
+          document.getElementById("oneWeekStreakBadge").style.display = "inline-block";
+        } else {
+          document.getElementById("oneWeekStreakBadge").style.display = "none";
+        }
+
+        if (hasTwoWeekStreak) {
+          console.log("2 week streak");
+          document.getElementById("twoWeekStreakBadge").style.display = "inline-block";
+        } else {
+          document.getElementById("twoWeekStreakBadge").style.display = "none";
+        }
+      })
+      .catch(error => {
+        console.log("Error getting ratings: ", error);
+      });
   } else {
-    console.log("User does not exist in Firestore.");
+    console.log("User is not logged in.");
   }
-}).catch(error => {
-  console.log("Error getting user document: ", error);
-});
-
+}
